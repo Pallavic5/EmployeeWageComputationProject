5bb15d0412f9f73283356cca9fce48f7ba611858
Pallavic5 <pchedge235@gmail.com>
2022-08-27 18:18:26 +0530

    Added: Calculate Daily Employee Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,17 +6,24 @@ public class EmployeeWage {
 		
 		System.out.println("Welcome to Employee Wage Computation Program.");	
 		
-		final int IS_FULL_TIME = 1;
-		int  empCheck = (int)(Math.random() * 100) % 2;
-		if (empCheck == IS_FULL_TIME) {
+		final int IS_FULL_TIME = 1;	
+		final int WAGE_PER_HOUR = 20;
+		
+		int empFullDayWrkHrs = 0;
+		int empCheck = (int) (Math.random() * 100) % 2;
+		
+		if(empCheck == IS_FULL_TIME) {
 			
 			System.out.println("Employee is Present");
+			empFullDayWrkHrs = 8;
 			
 		}
 		else {
-				System.out.println("Employee is Absent");
-		}
-
+				 System.out.println("Employee is Absent");
+		}	
+		
+		int empWage = empFullDayWrkHrs * WAGE_PER_HOUR;
+		System.out.println("Employee Daily Wage is " + empWage);
 	}
 
 }

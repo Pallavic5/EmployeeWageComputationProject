7f5dfb8097b865abaa8e7559b54d2f4c2dcb386b
Pallavic5 <pchedge235@gmail.com>
2022-08-27 18:47:16 +0530

    Added: Calculating Wages for a month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,27 +9,33 @@ public class EmployeeWage {
 		final int IS_PART_TIME = 1;
 		final int IS_FULL_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
+		final int WORKING_DAYS = 20;
 		
-		int empCheck = (int) (Math.random() * 100) % 3;
-		int empFullDayWrkHrs = 0;
-		switch(empCheck)
-		{
-		case IS_FULL_TIME :
-            System.out.println("Employee is Present Full time");
-            empFullDayWrkHrs = 16;
-            break;
-            
-        case IS_PART_TIME:
-            System.out.println("Employee is Present Part time");
-            empFullDayWrkHrs = 8;
-            break;
-            
-        default:
-            System.out.println("Employee is Absent");  
-		}
+		int totalWage = 0;
 		
-		int empWage = WAGE_PER_HOUR * empFullDayWrkHrs;
-		System.out.println("Employee Daily Wage is " + empWage);
+		for(int day = 1; day <= WORKING_DAYS; day++ ) {
+			
+			int empCheck = (int) (Math.random() * 100) % 3;
+			int empFullDayWrkHrs = 0;
+			
+			switch(empCheck) {
+			 case  IS_FULL_TIME :
+				  empFullDayWrkHrs = 16;
+	              break;
+	            
+	         case  IS_PART_TIME:
+	        	  empFullDayWrkHrs = 8;
+	              break;
+	             
+	          default:
+			}
+			
+			int empWage = empFullDayWrkHrs * WAGE_PER_HOUR;
+            System.out.println("Day " + day + " wage is:" + empWage);
+            totalWage += empWage;
+        }
+		
+         System.out.println("Total wage for a month = " + totalWage);   
 	}
 
 }
